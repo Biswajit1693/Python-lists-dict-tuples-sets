5b47e33c0e3c2cda4a272206797fc64ac015fddc
BISWAJIT  PATNAIK <112742314+Biswajit1693@users.noreply.github.com>
2022-10-18 06:49:35 +0530

    Add files via upload

---
+++ listspy.py
@@ -0,0 +1,16 @@
+a=[1,3,4,54,5,6,"hvsh,h"]
+print(a)
+#list indexing & slicing
+print(a[2])
+print(a[::])
+print(a[1:])
+print(a[:-1])
+print(a[:4:2])
+print(a[2:6:3])
+print(a[0])
+
+x=str(5)#type conersion of data type
+print(str(x))
+print(type(x))
+z=[1,3,4,2,3]
+#print(z[z>3])#it shows erroe as we cannpt do the comparison on list like this on numpy array we can do
